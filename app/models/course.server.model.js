698199330eb00d493489c9dde8d69b8ba00a8044
Nurul Huda Robin <robin@dexsa.org>
2014-12-16 02:57:36 +0600

    course updated

--- app/models/course.server.model.js
+++ app/models/course.server.model.js
@@ -22,6 +22,34 @@ var CourseSchema = new Schema({
 		required:'Please fill Course Code',
 		trim:true
 	},
+	category:{
+		type: String,
+		default: '',
+		trim:true
+	},
+	section:{
+		type: String,
+		default: '',
+		trim:true
+	},
+	semester:{
+		type: String,
+		default: '',
+		trim:true
+	},
+	day: {
+		type: String,
+		default: "",
+		trim:true
+	},
+	startTime: {
+		type: Date,
+		default: Date.now
+	},
+	endTime: {
+		type: Date,
+		default: Date.now
+	},
 	created: {
 		type: Date,
 		default: Date.now
--- public/modules/courses/controllers/courses.client.controller.js
+++ public/modules/courses/controllers/courses.client.controller.js
@@ -2,67 +2,79 @@
 
 // Courses controller
 angular.module('courses').controller('CoursesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Courses',
-	function($scope, $stateParams, $location, Authentication, Courses) {
-		$scope.authentication = Authentication;
+    function ($scope, $stateParams, $location, Authentication, Courses) {
+        $scope.authentication = Authentication;
 
-		// Create new Course
-		$scope.create = function() {
-			// Create new Course object
-			var course = new Courses ({
-				title: this.title,
-				courseCode: this.courseCode
-			});
+        // Create new Course
+        $scope.create = function () {
+            // Create new Course object
+            var course = new Courses({
+                title: this.title,
+                courseCode: this.courseCode,
+                category: this.category,
+                section: this.section,
+                semester: this.semester,
+                day: this.day,
+                startTime: this.startTime,
+                endTime: this.endTime
+            });
 
-			// Redirect after save
-			course.$save(function(response) {
-				$location.path('courses/' + response._id);
+            // Redirect after save
+            course.$save(function (response) {
+                $location.path('courses/' + response._id);
 
-				// Clear form fields
-				$scope.title = '';
-				$scope.courseCode='';
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+                // Clear form fields
+                $scope.title = '';
+                $scope.courseCode = '';
+                $scope.category = '';
+                $scope.section = '';
+                $scope.semester = '';
+                $scope.day = '';
+                $scope.startTime = Date.now
+                $scope.endTime = Date.now;
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-		// Remove existing Course
-		$scope.remove = function(course) {
-			if ( course ) { 
-				course.$remove();
+        // Remove existing Course
+        $scope.remove = function (course) {
+            if (course) {
+                course.$remove();
 
-				for (var i in $scope.courses) {
-					if ($scope.courses [i] === course) {
-						$scope.courses.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.course.$remove(function() {
-					$location.path('courses');
-				});
-			}
-		};
+                for (var i in $scope.courses) {
+                    if ($scope.courses [i] === course) {
+                        $scope.courses.splice(i, 1);
+                    }
+                }
+            } else {
+                $scope.course.$remove(function () {
+                    $location.path('courses');
+                });
+            }
+        };
 
-		// Update existing Course
-		$scope.update = function() {
-			var course = $scope.course;
+        // Update existing Course
+        $scope.update = function () {
+            var course = $scope.course;
 
-			course.$update(function() {
-				$location.path('courses/' + course._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+            course.$update(function () {
+                $location.path('courses/' + course._id);
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-		// Find a list of Courses
-		$scope.find = function() {
-			$scope.courses = Courses.query();
-		};
+        // Find a list of Courses
+        $scope.find = function () {
+            $scope.courses = Courses.query();
+        };
 
-		// Find existing Course
-		$scope.findOne = function() {
-			$scope.course = Courses.get({ 
-				courseId: $stateParams.courseId
-			});
-		};
-	}
+        // Find existing Course
+        $scope.findOne = function () {
+            $scope.course = Courses.get({
+                courseId: $stateParams.courseId
+            });
+        };
+    }
 ]);
